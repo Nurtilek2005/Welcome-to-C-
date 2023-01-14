451726a1f87c7edf6ae10f630225de2a200d4407
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-14 23:22:08 +0600

    finished homeWork_1's task_8

--- homeWork_1/task_8/Program.cs
+++ homeWork_1/task_8/Program.cs
@@ -4,3 +4,37 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+string? strNum = null;
+int number = 0;
+
+Console.WriteLine("Введите натуральное число: ");
+strNum = Console.ReadLine();
+
+if (strNum != null && strNum != "")
+{
+    number = int.Parse(strNum);
+    if (number > 0)
+    {
+        int currentNum = 1;
+        while (currentNum <= number)
+        {
+            if ((currentNum % 2) == 0)
+            {
+                Console.Write(currentNum);
+                if (currentNum < (number - 1)) {
+                    Console.Write(", ");
+                }
+            }
+            currentNum++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ваше число не натуральное!");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ничего не ввели!");
+}
