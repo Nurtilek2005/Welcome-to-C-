9cda01813ed3a257ee15c675535d96d4a11bcfb0
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-14 23:10:47 +0600

    finished homeWork_1's task_6

--- homeWork_1/task_6/Program.cs
+++ homeWork_1/task_6/Program.cs
@@ -6,3 +6,27 @@
 7 -> нет
 */
 
+string? strNum = null;
+int number = 0;
+
+Console.WriteLine("Введите число, что бы узнать, является ли число чётным: ");
+strNum = Console.ReadLine();
+
+if (strNum != null && strNum != "")
+{
+    number = int.Parse(strNum);
+    Console.Write("Ваше число: ");
+    Console.Write(number);
+    if ((number % 2) == 0)
+    {
+        Console.WriteLine("является чётным");
+    }
+    else
+    {
+        Console.WriteLine("не чётный");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ничего не ввели!");
+}
