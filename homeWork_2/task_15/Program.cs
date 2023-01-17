251d1a21aa2cc34d4f0e9559b2d97716625351b9
Nurtilek <nurtileks2005@gmail.com>
2023-01-17 19:08:37 +0600

    added homeWork_2's task_15

--- homeWork_2/task_15/Program.cs
+++ homeWork_2/task_15/Program.cs
@@ -5,3 +5,46 @@
 7 -> да
 1 -> нет
 */
+
+string? strNum = null;
+int dayOfWeek = 0;
+
+Console.Write("Введите число, для того что бы получить третью цифру: ");
+strNum = Console.ReadLine();
+
+if (strNum != null && strNum != "")
+{
+    dayOfWeek = int.Parse(strNum);
+    string dayOfWeekName = GetDayOfTheWeek(dayOfWeek);
+    if (dayOfWeek == 6 || dayOfWeek == 7)
+    {
+        Console.WriteLine($"Да, cегодня выходной! {dayOfWeekName}!");
+    }
+    else if (dayOfWeek >= 1 && dayOfWeek <= 5)
+    {
+        Console.WriteLine($"Нет, cегодня рабочий день. {dayOfWeekName}.");
+    }
+    else
+    {
+        Console.WriteLine($"Нет, ошибка! Сегодня день {dayOfWeekName}.");
+    }
+}
+else
+{
+    Console.WriteLine("Введите корректное число!");
+}
+
+// Надеюсь что функции разрешены в домашке...
+// Если что, извините. Просто не знал.
+
+string GetDayOfTheWeek(int dayOfTheWeek)
+{
+    if (dayOfTheWeek == 1) return "Понедельник";
+    else if (dayOfTheWeek == 2) return "Вторник";
+    else if (dayOfTheWeek == 3) return "Среда";
+    else if (dayOfTheWeek == 4) return "Четверг";
+    else if (dayOfTheWeek == 5) return "Пятница";
+    else if (dayOfTheWeek == 6) return "Суббота";
+    else if (dayOfTheWeek == 7) return "Воскресенье";
+    else return "404";
+}
