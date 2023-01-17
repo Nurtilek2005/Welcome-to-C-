5775078ee4ba83505c1e5896e55df5f3a5572a84
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-17 14:27:41 +0600

    added seminar_2's task_3

---
+++ seminar_2/task_11/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача №11
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randNum = new Random().Next(100, 1000);
+
+int firstNum = randNum / 100;
+int lastNum = randNum % 100;
+int number = firstNum * 10 + lastNum;
+
+Console.WriteLine($"Было сгенерировано число: {randNum}.");
+Console.WriteLine($"После удаления второго числа: {number}.");
---
+++ seminar_2/task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача №3
+Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+*/
+
+string? firstNumStr = null;
+string? secondNumStr = null;
+
+Console.Write("Введите первое число: ");
+firstNumStr = Console.ReadLine();
+if (firstNumStr != null && firstNumStr != "")
+{
+    int firstNum = int.Parse(firstNumStr);
+    Console.Write("Введите второе число: ");
+    secondNumStr = Console.ReadLine();
+    if (secondNumStr != null && secondNumStr != "")
+    {
+        int secondNum = int.Parse(secondNumStr);
+        int res = firstNum % secondNum;
+        if (res == 0) {
+            Console.WriteLine($"Число {firstNum} кратный числу {secondNum}");
+        } else {
+            Console.WriteLine($"Число {firstNum} кратный числу {secondNum}. Остаток: {res}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ничего не ввели в второе число!");
+    }
+
+}
+else
+{
+    Console.WriteLine("Вы ничего не ввели в первое число!");
+}
