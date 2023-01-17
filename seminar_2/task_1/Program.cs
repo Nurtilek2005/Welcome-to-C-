cad49caade7fbf39a676937b4ab9575f2da3dda9
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-17 13:36:17 +0600

    added seminar_2 and task_1

---
+++ seminar_2/task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача №1
+Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+*/
+
+int randNumber = new Random().Next(10, 100);
+int number1 = randNumber / 10;
+int number2 = randNumber % 10;
+
+int max = number1;
+if (number2 > max) max = number2;
+
+Console.Write("Max number of numbers: ");
+Console.Write(number1 + ", " + number2);
+Console.WriteLine(" - " + max + "!");
+
