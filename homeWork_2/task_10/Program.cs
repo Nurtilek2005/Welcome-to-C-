18936f8fb4274db1273ca5b96ab8c901d654da5d
Nurtilek <nurtileks2005@gmail.com>
2023-01-17 18:40:23 +0600

    added homeWork_2's task_10

---
+++ homeWork_2/task_10/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+string? strNum = null;
+int number = 0, midNum = 0;
+
+Console.Write("Введите трёхзначное число: ");
+strNum = Console.ReadLine();
+
+if (strNum != null && strNum != "")
+{
+    number = int.Parse(strNum);
+    if (number < 1000 && number >= 100)
+    {
+        midNum = (number / 10) % 10;
+        Console.WriteLine($"Второе число: {midNum}");
+    }
+    else
+    {
+        Console.WriteLine("Важе число не трёхзначное!");
+    }
+}
+else
+{
+    Console.WriteLine("Введите корректное число!");
+}
