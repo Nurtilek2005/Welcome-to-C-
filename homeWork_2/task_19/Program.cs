dcb04ddfb170dd6de39584fa65acd523d75c4a67
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-21 16:35:23 +0600

    extended homeWork_2

---
+++ homeWork_2/task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+int GetNumber(string promptText)
+{
+    Console.Write(promptText + ": ");
+    string? strNum = Console.ReadLine();
+    if (strNum == null || strNum.Trim() == "")
+    {
+        Console.WriteLine("Вы ничего не ввели!");
+        return 0;
+    }
+    return int.Parse(strNum);
+}
+
+int checkNumber()
+{
+    
+}
+
+int nuber = GetNumber("Введите пятизначную цифру");
+
