b35dc9cad00b4c6f20a379f95650342d26e281e8
Nurtilek <nurtileks2005@gmail.com>
2023-01-23 13:38:44 +0600

    added homeWork_2's task_23

--- homeWork_2/task_23/Program.cs
+++ homeWork_2/task_23/Program.cs
@@ -5,3 +5,35 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+double CubeOfNumber(int number)
+{
+    return Math.Pow(number, 3);
+}
+
+void FetchCubes(int number)
+{
+    Console.Write("Таблица кубов чисел: ");
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write(CubeOfNumber(i) + ", ");
+    }
+    Console.Write(CubeOfNumber(number));
+}
+
+
+int GetNumber(string promptText)
+{
+    Console.Write(promptText + ": ");
+    string? strNum = Console.ReadLine();
+    if (strNum == null || strNum.Trim() == "")
+    {
+        Console.WriteLine("Вы ничего не ввели!");
+        return 0;
+    }
+    return int.Parse(strNum);
+}
+
+int number = GetNumber("Введите натуральное число");
+
+FetchCubes(number);
