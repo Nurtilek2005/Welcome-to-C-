bba56e15cb7e0f46d2a614bdba1df838abc49256
Nurtilek <nurtileks2005@gmail.com>
2023-01-23 13:13:32 +0600

    added homeWork_2's task_19

--- homeWork_2/task_19/Program.cs
+++ homeWork_2/task_19/Program.cs
@@ -19,10 +19,25 @@ int GetNumber(string promptText)
     return int.Parse(strNum);
 }
 
-int checkNumber()
+void CheckNumber(int number)
 {
-    
+    if (!(number > 9999 && number < 100000))
+    {
+        Console.WriteLine("Ваше число не 5 значное!");
+        return;
+    }
+    int firstNumber = number / 10000;
+    int secondNumber = number % 10000 / 1000;
+    int fourthNumber = number % 100 / 10;
+    int fifthNumber = number % 10;
+    if (firstNumber == fifthNumber && secondNumber == fourthNumber)
+    {
+        Console.WriteLine($"Да! Число: {number} является палиндромом.");
+    } else {
+        Console.WriteLine($"Нет! Число: {number} не является палиндромом.");
+    }
 }
 
-int nuber = GetNumber("Введите пятизначную цифру");
+int number = GetNumber("Введите пятизначную цифру");
 
+CheckNumber(number);
