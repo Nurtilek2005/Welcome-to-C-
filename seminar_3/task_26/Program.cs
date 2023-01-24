c31a1c95fad98543d5ce08ab09fdf93896743b31
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-24 14:02:34 +0600

    added seminar_3 && task_24 and task_26

---
+++ seminar_3/task_26/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetNumbersCount(int num)
+{
+    int count = 1;
+    for (int i = num; i >= 10; i = i / 10)
+    {
+        count++;
+    }
+    return count;
+}
+
+int PromptNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = PromptNumber("Введите число чтобы узнать кол-во цифр: ");
+Console.WriteLine(number + " -> " + GetNumbersCount(number));
