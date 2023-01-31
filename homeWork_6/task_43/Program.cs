e44bdcfc503f7c5787cb001c2afda28f75196039
Nurtilek2005 <nurtileks2005@gmail.com>
2023-01-31 21:03:39 +0600

    finished homeWork_6's task_43

--- homeWork_6/task_43/Program.cs
+++ homeWork_6/task_43/Program.cs
@@ -6,3 +6,39 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+
+/* --- Получение данных от пользователя ----------- */
+string GetCrossPoint(int b1, int k1, int b2, int k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    return $"({x}; {y})";
+}
+/* ------------------------------------------------ */
+
+/* --- Получение данных от пользователя ----------- */
+int PromptNumber(string message)
+{
+    Console.Write(message);
+    string? line = Console.ReadLine();
+    if (line == null || line == "")
+    {
+        Console.WriteLine("Вы ничего не ввели!");
+        return 0;
+    }
+    return int.Parse(line);
+}
+/* ------------------------------------------------ */
+
+int b1 = PromptNumber("Введите значение b1: ");
+int k1 = PromptNumber("Введите значение k1: ");
+int b2 = PromptNumber("Введите значение b2: ");
+int k2 = PromptNumber("Введите значение k2: ");
+
+/* ------------------------------------------------ */
+
+Console.Write("b1 = " + b1 + ", ");
+Console.Write("k1 = " + k1 + ", ");
+Console.Write("b2 = " + b2 + ", ");
+Console.Write("k2 = " + k2 + "; -> ");
+Console.WriteLine(GetCrossPoint(b1, k1, b2, k2));
