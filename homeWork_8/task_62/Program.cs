acbcc90c139b2ab248e163260fb5fd4b02be1601
Nurtilek <nurtileks2005@gmail.com>
2023-02-15 11:53:19 +0600

    fixed homeWork_8's task_62

--- homeWork_8/task_62/Program.cs
+++ homeWork_8/task_62/Program.cs
@@ -8,9 +8,6 @@
 */
 
 using MatrixMaster;
-using System.Globalization;
-
-Matrix<int> matrix = new Matrix<int>(4, 4);
 
 void SpiralFillMatrix(Matrix<int> matrix, int direction = 1, int i = 0, int row = 0, int col = 0, int k = 0)
 {
@@ -18,22 +15,22 @@ void SpiralFillMatrix(Matrix<int> matrix, int direction = 1, int i = 0, int row 
     int columns = matrix.GetColumnsLength();
     int size = rows * columns;
     if (i == size) return;
-    Console.Write($"{i}: ({row}, {col}) -> ");
-    Console.WriteLine($"direction: {direction}");
-    matrix[row, col] = i;
+    matrix[row, col] = i + 1;
     switch (direction)
     {
         case 1:
-            if (col == columns - 1 - k)
+            if (col == (columns - k) - 1)
             {
+                i--;
                 direction = 2;
                 break;
             }
             col++;
             break;
         case 2:
-            if (row == rows - 1 - k)
+            if (row == (rows - k) - 1)
             {
+                i--;
                 direction = 3;
                 break;
             }
@@ -42,6 +39,8 @@ void SpiralFillMatrix(Matrix<int> matrix, int direction = 1, int i = 0, int row 
         case 3:
             if (col - k == 0)
             {
+                k++;
+                i--;
                 direction = 4;
                 break;
             }
@@ -50,12 +49,9 @@ void SpiralFillMatrix(Matrix<int> matrix, int direction = 1, int i = 0, int row 
         case 4:
             if (row - k == 0)
             {
+                i--;
                 direction = 1;
                 break;
-            }
-            if (matrix[row, col] != 0)
-            {
-                k++;
             }
             row--;
             break;
@@ -81,6 +77,8 @@ void PrettyPrintMatrix(Matrix<int> matrix, int width = 3)
         Console.WriteLine();
     }
 }
+
+Matrix<int> matrix = new Matrix<int>(4, 5);
 
 SpiralFillMatrix(matrix);
 PrettyPrintMatrix(matrix, 2);
